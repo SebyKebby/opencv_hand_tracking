fbfde628160f685a46fedfe8d4f281ca4b5aa638
SebbyKebby <109518181+SebyKebby@users.noreply.github.com>
2025-02-12 17:48:08 +0700

    Create a system to interpret hand motion

--- streamlined_mp_opencv(use_legacy_mp).py
+++ streamlined_mp_opencv(use_legacy_mp).py
@@ -3,13 +3,31 @@
 #       Implement mediapipe (done)
 #       Implement handtracking(done)
 #       Implement overlay (done)
+#       Create a system to interpret hand motion (done)
 #       Convert handtracked motion to keyboard input
 #       Do the deed
 
-
+import pyautogui
 import cv2
 import mediapipe as mp
 import time
+
+def count_fingers(lst):
+    cnt = 0
+
+    thresh = (lst.landmark[0].y*100 - lst.landmark[9].y*100) / 2
+
+    if (lst.landmark[5].y*100 - lst.landmark[8].y*100) > thresh :
+            cnt +=1
+    if (lst.landmark[9].y*100 - lst.landmark[12].y*100) > thresh :
+            cnt +=1
+    if (lst.landmark[13].y*100 - lst.landmark[16].y*100) > thresh :
+            cnt +=1
+    if (lst.landmark[17].y*100 - lst.landmark[20].y*100) > thresh :
+        cnt +=1
+    if (lst.landmark[4].y*100 - lst.landmark[5].y*100) > 5 :
+          cnt +=1
+    return cnt
 
 cap = cv2.VideoCapture(0)
 
@@ -30,7 +48,12 @@ while True:
     # print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, results.multi_hand_landmarks[0], mpHands.HAND_CONNECTIONS)
+            
+            hand_keyPoints = results.multi_hand_landmarks[0]
+
+            print(count_fingers(hand_keyPoints))
+
+            mpDraw.draw_landmarks(img, hand_keyPoints, mpHands.HAND_CONNECTIONS)
 
 
     cTime = time.time()
